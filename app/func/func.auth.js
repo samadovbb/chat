5e65cc7f313943ade8d8f939e686d1b74bd1c842
mensenvau <otkir8311@gmail.com>
2021-06-29 09:49:27 +0500

    hi

---
+++ app/func/func.auth.js
@@ -0,0 +1,9 @@
+let AuthUsers = (hash) => {
+    return false;
+}
+
+
+
+module.exports = {
+    AuthUsers: AuthUsers
+}
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express = require("express")
-const app = express()
-const fileUpload = require("express-fileupload")
+const server = require('http').createServer();
+// socket io connect .....
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*'
+    }
+})
+
 const config = require("./app/config/index.json").app
 const chalk = require('chalk');
 const PORT = config.PORT || 5050
 
-
 // parse application/x-www-form-urlencoded and parse application ... and public .... and file yuklash ... and ejs ....
-app.use(express.urlencoded({ extended: false })).use(express.json()).use(express.static('public')).use(fileUpload())
+//app.use(express.urlencoded({ extended: false })).use(express.json()).use(express.static('public'))
+const { AuthUsers } = require("./app/func/func.auth")
+
+io.on('connection', (socket) => {
+    let hash = socket.handshake.query.hash
+
+    // is AuthUsers ...
+    if (AuthUsers(hash)) {
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+    } else {
+        console.log(hash)
+        io.emit("error", "hi")
+    }
+
+});
 
 
-// create app ;
-app.listen(PORT, () => {
-    console.log(chalk.blue(`Create server http://localhost:${PORT} .... click url`))
+// create app ; 
+server.listen(PORT, () => {
+    console.log(`Server ready. Port: ${PORT}`)
 })
